7198df69137b935b47eef6ace316ddf3f82141cd
Sebastian <sebastian.morales01@uceva.edu.co>
2025-02-28 04:11:56 +0000

    Archivos iniciales

---
+++ aplicacion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Arreglo de strings: char **
+
+//La funcion main es el entry point (punto de entrada) de un ejecutable en lenguaje c
+int main(void){
+ printf("\nHola mundo!\n");
+ return 0;
+}
