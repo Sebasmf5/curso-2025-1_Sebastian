66c5ac05995a06143b73e10102d382933d3d059e
Sebastian <sebastian.morales01@uceva.edu.co>
2025-03-21 16:19:41 +0000

    Flotantes

---
+++ ejemplo-7/ejemplo-7.s
@@ -0,0 +1,14 @@
+.text
+.global suma_float
+
+
+# Para los floats se utiliza los registros xmm (son de 128 bits)
+# Segun System V:
+# xmm0 funcion (xmm0, xmm1, xmm2, ...) En ensamblador
+# float funcion (float1, float2, float3, ...) En lenguaje C
+suma_float:
+  addss %xmm1, %xmm0  # xmm0 = xmm0 + xmm1
+  ret
+
+
+# addss: Add Scalar Single preciion (sumar un escalar de precision simple, es decir float>
