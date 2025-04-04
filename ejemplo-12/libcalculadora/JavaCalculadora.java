84b620b83a8dde2f86ddccb36ed14ba5a3f54b1f
Sebastian <sebastian.morales01@uceva.edu.co>
2025-04-04 16:31:33 +0000

    Java y libreria dinamica

---
+++ ejemplo-12/libcalculadora/JavaCalculadora.java
@@ -0,0 +1,10 @@
+package libcalculadora;
+
+
+public class JavaCalculadora {
+    public native int sumar(int numero1, int numero2);
+    public native int restar(int numero1, int numero2);
+    public native int multiplicar(int numero1, int numero2);
+    public native float division(int numero1, int numero2);
+     
+}
