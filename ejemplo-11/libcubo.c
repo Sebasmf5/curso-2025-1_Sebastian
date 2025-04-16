b53b57883215617b20f25314883f5de34f2604ac
Sebastian <sebastian.morales01@uceva.edu.co>
2025-04-16 13:21:28 +0000

    ejemplo-11

---
+++ ejemplo-11/libcubo.c
@@ -0,0 +1,11 @@
+// Signature de las funciones
+double calcularCubo(double numero){
+    double resultado;
+    return resultado = numero * numero * numero;
+}
+
+int multiplicarPorOcho(int numero){
+    int resultado;
+    resultado = numero << 3; //0000 1000
+    return resultado;
+}
